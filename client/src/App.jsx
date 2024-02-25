8af728117039405d482dae0ff1b199f0886843b3
Zeba Katarki <zkatarki@gmail.com>
2024-02-25 13:08:12 +0530

    Ceated pages routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
 
 export default function App() {
   return (
-    <div>
-      <h1  className='text-8xl'>Hello</h1>
-    </div>
+    <BrowserRouter>
+    <Routes>
+
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/dashboard" element={<Dashboard/>} />
+      <Route path="/projects" element={<Projects/>} />
+      <Route path="/signin" element={<Signin/>} />
+      <Route path="/signup" element={<Signup/>} />
+
+    </Routes>
+    </BrowserRouter>
   )
 }
