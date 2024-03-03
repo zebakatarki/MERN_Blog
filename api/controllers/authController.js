1f2378c460fb3eb23f89629a1ad9167961c8c3ce
Zeba Katarki <zkatarki@gmail.com>
2024-03-03 18:45:24 +0530

    Added UI and Functionality to the SignIn page

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,6 +1,9 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import  jwt  from "jsonwebtoken";
+// import pkg from "jsonwebtoken";
+// const {jwt} = pkg;
 
 export const signup = async (req, res, next) =>{
     console.log(req.body);
@@ -29,5 +32,140 @@ export const signup = async (req, res, next) =>{
         // res.status(500).json({message: error.message});
         next(error);
     }
-
 };
+
+export const signin = async(req,res,next)=>{
+    const {email, password} = req.body;
+
+    if(!email || !password || email === "" || password ===  ""){
+        next (errorHandler(400,"Api tool All fields are required"));
+    }
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404, "Invalid Email"));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword){
+            return next(errorHandler(400,"Invalid Password"));
+        }    
+        const token = jwt.sign(
+            {id: validUser._id}, process.env.JWT_SECRET
+        ); 
+
+        //Sepreates the password from the rest of information we can see the result without password on iosomnia
+        const {password: pass, ...rest} = validUser._doc; 
+
+        res.status(200).cookie('access_token', token, {
+            httpOnly : "true"}).json(rest);
+    }catch(error){
+        next(error);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// JWT Token Generation:
+// The jwt.sign() function generates a JWT token. It takes a payload (in this case, an object containing the user's ID)
+// and signs it using the provided secret key (process.env.JWT_SECRET). This step ensures that the token is 
+// cryptographically secure and can be verified by the server later.
+
+// Setting HTTP Response Status:
+// The res.status(200) function sets the HTTP response status code to 200, indicating that the request has been 
+// successfully processed.
+
+// Setting Access Token Cookie:
+// The .cookie() function sets a cookie named 'access_token' in the HTTP response. The cookie contains the JWT token 
+// generated in the first step. Additionally, the { httpOnly: "true" } option ensures that the cookie is marked as 
+//HTTP 
+// only, meaning it cannot be accessed by client-side JavaScript. This enhances security by protecting the token from 
+// certain types of attacks like Cross-Site Scripting (XSS).
+
+// Sending JSON Response:
+// The .json() function sends a JSON response back to the client. It includes the validUser object, which likely 
+// represents the user data retrieved from the database during the authentication process. By sending this user data 
+// along with the token, the client-side application can have immediate access to information about the authenticated 
+// user, enabling personalized user experiences.
+
+// Completing the Response:
+// Once all response setup is completed, the server sends the HTTP response back to the client, containing the status 
+// code, cookie, and JSON payload.
+// Internally, the server-side framework (such as Express.js in Node.js) handles the processing of these response setup 
+// functions and ensures that the appropriate headers and content are included in the HTTP response. The framework 
+// abstracts away many of the lower-level details, allowing developers to focus on writing clean and maintainable code 
+// for handling authentication and generating responses.
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,9 +1,137 @@
-import React from 'react'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import React from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import { useState } from 'react';
 
-export default function Signin() {
+export default function SignIn() {
+
+    let [signin,setSignin]=useState({
+        email:"",
+        password:"",
+    });
+    let [errorMessage,setErrorMessage]=useState(null);
+    let [loading,setLoading]=useState(false);
+    const navigate = useNavigate();
+
+    let signinHandler=(event)=>{
+        // event.preventdefault();
+        let fieldId = event.target.id; //element name
+        let newValue=event.target.value; //entered value
+        console.log("Field Id",fieldId); //fullName or userName 
+        console.log("Value",newValue); //entered values
+
+        setSignin((currele)=>{
+            // currele[fieldId]=newValue;
+            console.log("setSignin updating the signup value");//after each entered word
+            currele[event.target.id]=event.target.value.trim();
+            console.log("Returning values");
+            return {...currele} //we cant make the changes in original obj so aft OR to keep previous changes safe
+        })
+    };
+
+    //After submission of form
+    let handleSubmit=async(event)=>{
+        event.preventDefault(); //all the inputs values remains stay without getting ghosted refreshing
+        
+        if( !signin.email || !signin.password ){
+          console.log("Self declared error");
+          return setErrorMessage('React Please Fill Out All Fields'); //not show the error on console
+        }
+
+        console.log(signin);
+        try{
+          setLoading(true);
+          setErrorMessage(null);
+          // This line sends an HTTP request to the /api/auth/signup endpoint using the fetch() function and 
+          //The response from the server will be stored in the variable res. 
+          console.log("fetching api");
+          const res=await fetch('/api/auth/signin',{
+            method:"POST",
+            headers:{'Content-Type' : 'application/json'},
+            body : JSON.stringify(signin),
+          });
+
+          // This line extracts the JSON body content from the response (res) using the .json() method. 
+          const data = await res.json();
+
+          //id duplicate entries are present backend 
+          if(data.success === false){
+            console.log("Data.Success",data.success)
+            // setLoading(false);
+            setErrorMessage('react : Invalid Cridentials'); //error on console
+          }
+          setLoading(false);
+        
+          if(res.ok){
+            console.log(res);
+            navigate('/');
+          }
+        }catch(error){
+          //Client side error ex internet problem 
+          console.log(error);
+          setErrorMessage(error.message);
+          setLoading(false);
+        }
+    };
+
   return (
-    <div>
-      Signin
+    <div className='min-h-screen mt-40'>
+
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-7'>
+
+      <div className='flex-1'>
+      <Link to="/" className='font-bold dark:text-white text-4xl'>
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to bg-pink-500 rounded-lg text-white'>Zeba's</span>
+        Blog
+      </Link>
+
+      <p className='text-sm mt-5'>
+        This is my mern stack project surely give me feedback of your experience on my instagram handle or linkedIn......
+      </p>
+      </div>
+
+      <div className='flex-1'>
+        <form className='flex flex-col gap-6' onSubmit={handleSubmit}>
+
+          {
+          errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )
+          }
+          
+          <div>
+            <Label value='Your email '/>
+            <TextInput type="email" placeholder='xyz@gmail.com' id='email' value={signin.email} onChange={signinHandler} name="email"/>
+          </div>
+
+          <div>
+            <Label value='Your password '/>
+            <TextInput type="password" placeholder='**********' id='password' value={signin.password} onChange={signinHandler} name="password"/>
+          </div>
+
+          <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+            {
+              loading ? (
+                <>
+                  <Spinner size='sm'/>
+                <span className='pl-3'>Loading....</span>
+                </>
+              ) : "Sign In"
+            }
+          </Button>
+        </form>
+
+        <div className='flex gap-2 text-sm mt-5'>
+          <span>Don't have an account?</span>
+          <Link to='/signup' className='text-blue-500'>
+            SignUp
+          </Link>
+        </div> 
+
+      </div>
+      </div> 
     </div>
   )
 }
