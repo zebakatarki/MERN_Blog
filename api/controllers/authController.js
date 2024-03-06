18d578d9f39c7d53877ca51fda257dcd5550ee89
Zeba Katarki <zkatarki@gmail.com>
2024-03-06 23:09:47 +0530

    Add OAuth Functionality

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -61,6 +61,37 @@ export const signin = async(req,res,next)=>{
     }catch(error){
         next(error);
     }
+};
+
+export const google = async (req,res,next) =>{
+    const {name, email, googlePhotoUrl} = req.body;
+    try{
+        const user = await User.findOne({email});
+        if(user){
+            const token = jwt.sign({id: user._id},process.env.JWT_SECRET);
+            const {password, ...rest} = user._doc; //Hiding password
+            res.status(200).cookie('access_token', token,{
+                httpOnly:true,
+            }).json(rest);
+        } else{
+            const generatedPassword = Math.random(36).slice(-8) + Math.random(36).slice(-8); //0.2345asdfghjkm
+            const hashedPassword = bcryptjs.hashSync(generatedPassword,10);
+            const newUser = new User({
+                username: name.toLowerCase().split(' ').join(' ') + Math.random().toString(9).slice(-4), //Zeba Katarki = zebakatarki987654
+                email,
+                password: hashedPassword,
+                profilePicture: googlePhotoUrl,
+            });
+            await newUser.save();
+            const token = jwt.sign({id:newUser._id}, process.env.JWT_SECRET);
+            const {password, ...rest} = newUser._doc;
+            res.status(200).cookie('access_token', token,{
+                httpOnly:true,
+            }).json(rest);
+        }
+    }catch(error){
+        next(error)
+    }
 }
 
 
