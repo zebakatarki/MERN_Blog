64ebec08e7815e0e65ea20ca92f55ecf8efdb8ff
Zeba Katarki <zkatarki@gmail.com>
2024-03-07 14:51:23 +0530

    comment dark mood functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,12 +2,15 @@ import { Avatar, Button, Dropdown, DropdownDivider, Navbar, TextInput } from 'fl
 import React from 'react';
 import {Link, useLocation} from 'react-router-dom'; //Link for routes and useLocation to active the links 
 import {AiOutlineSearch} from 'react-icons/ai';
-import {FaMoon} from 'react-icons/fa';
-import {useSelector} from 'react-redux';
+import {FaMoon, FaSun} from 'react-icons/fa';
+import {useSelector, useDispatch} from 'react-redux';
+import { toggleTheme } from '../redux/theme/themeSlice';
 
 export default function Header() {
   const path = useLocation().pathname;
-  const {currentUser} = useSelector(state => state.user)
+  const dispatch = useDispatch();
+  const {currentUser} = useSelector((state) => state.user);
+  const {theme} = useSelector((state) => state.theme);
   return (
     <Navbar className='border-b-2 h-19'>
       <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -30,8 +33,10 @@ export default function Header() {
       {/* in small size after menu and in medium or lager size at te end */}
       <div className='flex gap-2 md:order-2'>
       {/* hidden in all sizes but small screen overrides this and inline on small screen */}
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill> 
-          <FaMoon></FaMoon>
+        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={()=>dispatch(toggleTheme())}> 
+
+        {theme === 'light' ? <FaMoon/> : <FaSun/> }
+          
         </Button>
 
         {currentUser ? (
