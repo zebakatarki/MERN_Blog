d4327532d98fc3e944403367b9f361eec2cf4b6b
Zeba Katarki <zkatarki@gmail.com>
2024-04-30 17:37:30 +0530

    dd the functionality to the comment component

--- api/routes/commentRoute.js
+++ api/routes/commentRoute.js
@@ -2,9 +2,11 @@ import express from 'express';
 import {verifytToken} from '../utils/userVerify.js';
 import { createComment } from '../controllers/commentController.js';
 import { getPostComments } from '../controllers/commentController.js';
+import { likeComment } from '../controllers/commentController.js';
 const router = express.Router();
 
 router.post('/create',verifytToken, createComment);
-router.get('/getPostComments/:postId',getPostComments)
+router.get('/getPostComments/:postId',getPostComments);
+router.put('/likeComment/:commentId', verifytToken, likeComment);
 
 export default router;
