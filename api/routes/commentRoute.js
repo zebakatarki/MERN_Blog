24cbcea85de1f6b0f299f078801658b9a1413d5b
Zeba Katarki <zkatarki@gmail.com>
2024-04-30 13:54:19 +0530

    Show the comments

--- api/routes/commentRoute.js
+++ api/routes/commentRoute.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import {verifytToken} from '../utils/userVerify.js';
 import { createComment } from '../controllers/commentController.js';
+import { getPostComments } from '../controllers/commentController.js';
 const router = express.Router();
 
 router.post('/create',verifytToken, createComment);
+router.get('/getPostComments/:postId',getPostComments)
 
 export default router;
