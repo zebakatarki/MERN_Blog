0b45d57fb6df6261f7f62d5d7b15be4fb9752850
Zeba Katarki <zkatarki@gmail.com>
2024-04-30 21:17:44 +0530

    Added edit and like functionality to comment

--- api/routes/commentRoute.js
+++ api/routes/commentRoute.js
@@ -3,10 +3,12 @@ import {verifytToken} from '../utils/userVerify.js';
 import { createComment } from '../controllers/commentController.js';
 import { getPostComments } from '../controllers/commentController.js';
 import { likeComment } from '../controllers/commentController.js';
+import { editComment } from '../controllers/commentController.js';
 const router = express.Router();
 
 router.post('/create',verifytToken, createComment);
 router.get('/getPostComments/:postId',getPostComments);
 router.put('/likeComment/:commentId', verifytToken, likeComment);
+router.put('/editComment/:commentId', verifytToken, editComment);
 
 export default router;
