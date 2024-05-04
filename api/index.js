2ae6d21a1cb80d9b2aa8732325d57e76788971d4
Zeba Katarki <zkatarki@gmail.com>
2024-05-04 17:52:02 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -1,17 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
-
-//To access .env file in backend alos
-import dotenv from 'dotenv';
-dotenv.config();
-
 import userRoutes from './routes/userRoute.js';
 import authRoutes from './routes/authRoute.js';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/postRoute.js';
 import commentRoutes from './routes/commentRoute.js'
-
 import cookieParser from 'cookie-parser';
+import path from 'path';
+
+dotenv.config();
 
 mongoose.connect
     (process.env.MONGO).then(()=>{
@@ -21,6 +18,8 @@ mongoose.connect
     console.log(err);
 });
 
+const __dirname = path.resolve();
+
 const app= express();
 
 // we cannot able to send the json info directly from any api tool like Insomnia so it Allows json as the input of the backend
@@ -28,19 +27,20 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-// app.use((req,res,next)=>{
-//     console.log("random");
-//     next();
-// });
-
 app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
 app.use('/api/post',postRoutes);
-app.use('/api/comment',commentRoutes)
+app.use('/api/comment',commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*',(req,res) => {
+    res.sendFile(path.join(__dirname,'client', 'dist', 'index.html'));
+});
 
 //Backend like duplicate required errors etc
 app.use((err, req, res, next)=>{
-    console.log("Error handling middleware for both",err.message);
+    console.log(err.message);
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
     res.status(statusCode).json({
