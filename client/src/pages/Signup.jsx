25edb49490b64c679dcf6ef9b0bd89a3f76dd53b
Zeba Katarki <zkatarki@gmail.com>
2024-05-06 15:39:07 +0530

    added new cluster

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -7,7 +7,7 @@ export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [errorMessage, setErrorMessage] = useState(null);
   const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
+  const navigate = useNavigate(); 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
